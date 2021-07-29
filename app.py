038465fb1dd5ba84f983cd3d70334b48a37855fd
Etienne Michels <ebmichel@mtu.edu>
2021-07-29 11:47:10 -0400

    FIrst fix to graphs, basic functionality added

--- app.py
+++ app.py
@@ -19,52 +19,62 @@ def index():
 # Graphs page
 @app.route("/graphs")
 def graphs():
+    toDisplay=['pH','Dissolved Oxygen']
 
-    title1 = "Numbers over time"
+    
+    title1 = toDisplay[0]+" over time"
     xLabel1 = "Time"
     yLabel1 = "Number"
 
-    data1 = [
-        (1, 56),
-        (2, 45),
-        (3, 47),
-        (4, 95),
-        (5, 87),
-        (6, 81),
-        (7, 75)
-    ]
+    
     dataTime=[]
-    for Time in SensorReading.select().where(SensorReading.name=='pH').order_by(SensorReading.time):
+    for Time in SensorReading.select().where(SensorReading.name==toDisplay[0]).order_by(SensorReading.time):
          dataTime.append(Time.time.timestamp())
-         print(Time.time.strftime("%c"))
     dataData=[]
     print(dataTime)
-    for Data in SensorReading.select().where(SensorReading.name=='pH').order_by(SensorReading.time):
+    for Data in SensorReading.select().where(SensorReading.name==toDisplay[0]).order_by(SensorReading.time):
         dataData.append(Data.value)
     print(dataData)
-    time1 = dataTime
     values1 = dataData
-
-    title2 = "Numbers over time"
+    time1=[]
+    time1=dataTime
+    first=time1[0]
+    for i in range(len(time1)):
+        time1[i]=(time1[i]-first)/3600
+    toParse=[]
+    for i in range(len(dataTime)):
+        toParse.append((dataTime[i],values1[i]))
+        print(toParse[i])
+    title2 = toDisplay[1]+" over time"
     xLabel2 = "Time"
     yLabel2 = "Number"
 
-    data2 = [
-        (1, 54),
-        (2, 46),
-        (3, 48),
-        (4, 93),
-        (5, 84),
-        (6, 87),
-        (7, 78)
-    ]
 
-    time2 = [row[0] for row in data2]
-    values2 = [row[1] for row in data2]
+    dataTime2=[]
+    for Time2 in SensorReading.select().where(SensorReading.name==toDisplay[1]).order_by(SensorReading.time):
+         dataTime2.append(Time2.time.timestamp())
+    dataData2=[]
+    print(dataTime2)
+    for Data2 in SensorReading.select().where(SensorReading.name==toDisplay[1]).order_by(SensorReading.time):
+        dataData2.append(Data2.value)
+    print(dataData2)
+    values2 = dataData2
+    time2=[]
+    time2=dataTime2
+    first2=time2[0]
+    for i in range(len(time2)):
+        time2[i]=(time2[i]-first2)/3600
+    toParse2=[]
+    for i in range(len(dataTime2)):
+        toParse2.append((dataTime2[i],values2[i]))
+        print(toParse2[i])
 
-    return render_template("graphs.html", time=time1, values=values1, title=title1)
+    return render_template("graphs.html", Time=time1, Values=values1, Title=title1,Parsed=toParse,Time2=time2,Values2=values2,Title2=title2,Parsed2=toParse2)
 
-
+class dataPoints:
+   def __init__(self,time,value):
+      self.x=time
+      self.y=value
 # About page for project
 @app.route("/about")
 def about():
