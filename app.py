696d14c0fc208951c7aa3fa41d0f3fb78336b637
Etienne Michels <ebmichel@mtu.edu>
2021-08-14 18:40:02 -0400

    Graphs can now be added to the graphing page using a button, added new functions in app.py to achieve this and rewrote essentially all of graphs.html.

--- app.py
+++ app.py
@@ -10,38 +10,49 @@ import flask
 from flask import Flask, render_template, request
 from model import Sensor,SensorReading
 app = Flask(__name__)
-toDisplay=['pH','Dissolved Oxygen']
+toDisplay=[]
+devices=[]
+for dev in Sensor.select():
+    devices.append(dev.name)
+print("Loaded app.py")
+
 # Home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/update",methods=['GET','POST'])
+@app.route("/update/<graphID>",methods=['GET','POST'])
 
-def update():
+def update(graphID):
         if request.method== 'GET':
-                (time1,values1,title1,toParse)=graphsUpdate(toDisplay[0])
-                (time2,values2,title2,toParse2)=graphsUpdate(toDisplay[1])
-                #graph1=flask.request.form('DeviceDrop')
-                #console.log(graph1)
-                return json.dumps({'Time':time1,'Values':values1,'Title':title1, 'Parsed':toParse,'Time2':time2,'Values2':values2,'Title2':title2,'Parsed2':toParse2})
+                print(toDisplay)
+                (time,values,title,toParse)=graphsUpdate(toDisplay[int(graphID)])
+                return json.dumps({'Time':time,'Values':values,'Title':title, 'Parsed':toParse})
         if request.method == 'POST':
                 data=request.json
-                print(data)
-                for i in range(len(data)):
-                        toDisplay[i]=data[i]
+                print("data:"+data[0])
+                toDisplay[int(graphID)]=data[0]
                 return flask.jsonify(data)
+
+@app.route("/add",methods=['POST'])
+def add():
+        print(toDisplay)
+        data=request.json[0]
+        print(data)
+        if data==0:
+                toDisplay.clear()
+        if (len(toDisplay))==data:
+                toDisplay.append("pH")
+        return flask.jsonify(data)
                 
 # Graphs page
 @app.route("/graphs",methods=['GET','POST'])
+
 def graphs():
-    devices=[]
-    for dev in Sensor.select():
-        devices.append(dev.name)
     return render_template("graphs.html",Devices=devices)
     #(time1,values1,title1,toParse,time2,values2,title2,toParse2)=graphsUpdate()
     #return render_template("graphs.html", Time=time1, Values=values1, Title=title1,Parsed=toParse,Time2=time2,Values2=values2,Title2=title2,Parsed2=toParse2)
-def graphsUpdate(toDisplay="pH"):
+def graphsUpdate(toDisplay):
     
     
     
