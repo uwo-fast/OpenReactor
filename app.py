57a0f570054319de88984ecdf30746a4ce387823
finnhaft <fhafting@uwo.ca>
2022-06-22 17:54:14 -0400

    finn_work

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def innit_control():
     for dev in connected.cons:
         global feedbackModules
         #print(dev[8])
-        feedbackModules[dev[1]]=importlib.import_module(dev[8]["control"])
+        feedbackModules[dev[1]]=importlib.import_module("control")
         con=sensor.I2C(name=dev[1],units=dev[2],address=dev[0],form=dev[3],request_message=dev[4],delay=dev[5],read_length=dev[6],enabled=dev[7],params=dev[8],def_state=dev[9])
         I2C_con.append(con)
         con.reset_control()
@@ -216,7 +216,7 @@ threadHandle=threading.Thread()
 dataLock=threading.Lock()
 
 
-THREAD_TIME=30
+THREAD_TIME=1
 
 try:                                                                        #Checks if thread is running, may not actually do anything
     experimentThreadStop()
