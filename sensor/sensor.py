3dd537ddbd1b2e45d0df36f949e0e12dc287aada
etienne <ebmichel@mtu.edu>
2022-09-10 11:32:23 -0400

    Add ability to send string as req_msg

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,3 +1,4 @@
+from tokenize import String
 import busio
 from .model import model
 from .model.model import Sensor,SensorReading
@@ -102,6 +103,8 @@ class I2C:
             toWrite=self.req_msg
         else:
             toWrite=[self.req_msg]
+        if type(toWrite[0]) is str:
+            toWrite[0]=bytes(toWrite[0].encode())
         if len(toWrite) !=0:
             i2c.writeto(self.addr,bytes(toWrite),stop=False)
         result=bytearray(self.read_len)
