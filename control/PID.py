52fdd9677341275fa2ca1154fd2f1067cdf9dd48
Demo User <debian@beaglebone.localdomain>
2022-11-23 18:28:44 +0000

    pH control working, all components added for first bioreactor experiment

--- control/PID.py
+++ control/PID.py
@@ -1,9 +1,8 @@
-def PID(input,lastInput,setpoint,kp,ki,kd,er=0):
+def PID(input,lastInput,setpoint,kp,ki,kd,er=0,max=100,min=0):
     e=setpoint-input
     er+=e
     ei=input-lastInput
     out=kp*e+ki*er-kd*ei
-    if out>100:out=100
-    if out<0:out=0
+    if out>max:out=max
+    if out<min:out=min
     return out,er
-
