a7a3644eb385961bbbd693541bf362c301585468
Cam Brooks <cambrooks3393@gmail.com>
2024-11-18 12:21:16 -0500

    updated first time setup

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -8,20 +8,25 @@ git submodule update --init --recursive
 
 # Install needed dependencies
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y python3-venv python3-pip sqlite3 npm
+sudo apt install -y python3-venv sqlite3 npm
 sudo apt install -y libpq-dev python3-dev python-dev-is-python3
 sudo apt install -y build-essential
 sudo apt install -y postgresql-server-dev-all
 
 # Create and configure python virtual environment
 python3 -m venv .venv
+
+# Ensure pip is installed in the virtual environment
 source .venv/bin/activate
+python3 -m ensurepip --upgrade
 
-pip install --upgrade pip
-pip install wheel
-pip install --upgrade wheel
-pip install --upgrade setuptools
+# Update permissions
+sudo chmod -R a+rwx .venv
 
+# Upgrade pip, setuptools, and wheel
+pip install --upgrade pip setuptools wheel
+
+# Install required Python packages
 pip install -r requirements.txt
 pip install --upgrade adafruit-blinka adafruit-platformdetect
 
